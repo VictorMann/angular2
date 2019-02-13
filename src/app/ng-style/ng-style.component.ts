c5a255e6d0acf3e86f850a15cfb42a3374d6b771
Victor Mann <victorcmann@gmail.com>
2019-02-12 22:15:30 -0200

    criando c ng-style

---
+++ src/app/ng-style/ng-style.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { ThrowStmt } from '@angular/compiler';
+
+@Component({
+  selector: 'app-ng-style',
+  templateUrl: './ng-style.component.html',
+  styleUrls: ['./ng-style.component.css']
+})
+export class NgStyleComponent implements OnInit {
+
+  tamanho: number = 20;
+  valorFonte: string = this.tamanho + 'px';
+  validaFonte: boolean = false;
+  validaCor: boolean = false;
+
+  constructor() { }
+
+  ngOnInit() { }
+
+  incrementar()
+  {
+    this.tamanho ++;
+    this.valorFonte = this.tamanho + 'px';
+  }
+
+  mudaFonte()
+  {
+    this.validaFonte = !this.validaFonte;
+  }
+
+  mudaCor()
+  {
+    this.validaCor = !this.validaCor;
+  }
+
+}
